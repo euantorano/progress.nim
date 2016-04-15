0852ffd254173b10027db22a97de490586016c9d
Euan Torano <euantorano@gmail.com>
2016-04-15 17:08:07 +0100

    Added parameters to set the caps
    Also added an optional cap for the bar so the output is like `[=======>......]`.

--- progress.nim
+++ progress.nim
@@ -2,8 +2,11 @@
 
 from strutils import repeat, formatFloat, ffDecimal
 
-const COMPLETE_STR = "="
-const INCOMPLETE_STR = "."
+const DEFAULT_COMPLETE_STR = "="
+const DEFAULT_INCOMPLETE_STR = "."
+const DEFAULT_COMPLETE_HEAD = ">"
+const DEFAULT_LEFT_DELIM = "["
+const DEFAULT_RIGHT_DELIM = "]"
 
 type
   ProgressBar* = ref ProgressBarObj
@@ -11,15 +14,24 @@ type
     ## A progress bar with a given length and step
     complete: string
     incomplete: string
+    incompleteHead: string
+    leftDelim: string
+    rightDelim: string
     step: int
     width: int
     total: int
     current: int
   InvalidPositionError* = object of Exception
 
-proc newProgressBar*(total: int = 100, step: int = 1, width: int = 100, complete: string = COMPLETE_STR, incomplete: string = INCOMPLETE_STR): ProgressBar =
+proc newProgressBar*(total: int = 100, step: int = 1, width: int = 100, complete: string = DEFAULT_COMPLETE_STR,
+  incomplete: string = DEFAULT_INCOMPLETE_STR, incompleteHead: string = DEFAULT_COMPLETE_HEAD,
+  leftDelim: string = DEFAULT_LEFT_DELIM, rightDelim: string = DEFAULT_RIGHT_DELIM): ProgressBar =
   ## Create a new progress bar with a given `total`, `step` and `width`.
-  return ProgressBar(total: total, step: step, width: width, complete: complete, incomplete: incomplete, current: 0)
+  var head = incompleteHead
+  if incompleteHead == nil:
+    head = incomplete
+  return ProgressBar(total: total, step: step, width: width, complete: complete, incomplete: incomplete,
+    incompleteHead: head, leftDelim: leftDelim, rightDelim: rightDelim, current: 0)
 
 proc isComplete*(pb: ProgressBar): bool =
   ## Check whether the progress bar is complete.
@@ -40,17 +52,24 @@ proc percent*(pb: ProgressBar): float =
 proc print(pb: ProgressBar) {.raises: [IOError], tags: [WriteIOEffect].} =
   ## Print the progress bar to stdout.
   let position = pb.currentPosition()
-  let completeBar = pb.complete.repeat(position)
+
+  let isComplete = pb.isComplete()
+
+  var completeBar = pb.complete.repeat(position)
+  let capLength = len(pb.incompleteHead)
+  if not isComplete:
+    completeBar = completeBar[0..^capLength] & pb.incompleteHead
+
   let incompleteBar = pb.incomplete.repeat(pb.width - position)
   let percentage = formatFloat(pb.percent(), ffDecimal, 2) & "%"
 
-  write(stdout, "\r[" & completeBar & incompleteBar & "]" & " " & percentage)
+  write(stdout, "\r" & pb.leftDelim & completeBar & incompleteBar & pb.rightDelim & " " & percentage)
   flushFile(stdout)
 
-  if pb.isComplete():
+  if isComplete:
     stdout.writeLine("")
 
-proc start*(pb: ProgressBar) {. tags: [WriteIOEffect].} =
+proc start*(pb: ProgressBar) {.raises: [IOError], tags: [WriteIOEffect].} =
   ## Start the progress bar. This will write the empty (0%) bar to the screen, which may not always be desired.
   if pb.current == 0:
     pb.print()
